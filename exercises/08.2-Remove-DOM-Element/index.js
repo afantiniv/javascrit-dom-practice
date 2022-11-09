feb6a8917a4e522b646a0e4909b3c4ce077cbaf4
afantiniv <fantiniv95@gmail.com>
2022-11-09 03:27:31 +0000

    Ejercicios resueltos v2

--- exercises/08.2-Remove-DOM-Element/index.js
+++ exercises/08.2-Remove-DOM-Element/index.js
@@ -1,4 +1,3 @@
 let childs = document.querySelector("#parentLi").childNodes;
-
 let element = childs[3];
 element.parentNode.removeChild(element);
--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -1,2 +1,13 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 // your code here
+let selecList = document.querySelector("#mySelect");
+countries.forEach(element => {
+    let country = document.createElement("option");
+    country.innerHTML = element;
+    selecList.appendChild(country);
+});
+
+document.querySelector("#mySelect").addEventListener("change", ()=>{
+    let x = document.getElementById("mySelect");
+    alert(x.options[x.selectedIndex].value);
+});
